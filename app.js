13ebff0202e95c17b5fc67d0283369b42ebd40bc
wasabipesto <21313833+wasabipesto@users.noreply.github.com>
2023-03-24 16:10:14 -0400

    add maintenance job

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const logger = require('morgan')
 const dashboardRouter = require('./routes/dashboard.js')
 const claimRouter = require('./routes/claim.js')
 const submitRouter = require('./routes/submit.js')
+const statsJob = require('./scheduled/stats.js')
 
 const app = express()
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.use('/api/dashboard', dashboardRouter)
 app.use('/api/claim', claimRouter)
 app.use('/api/submit', submitRouter)
+statsJob.job()
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
