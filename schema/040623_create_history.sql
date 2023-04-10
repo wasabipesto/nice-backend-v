b0e923bf92cf70b33158ae24219fabcedb981a2e
wasabipesto <21313833+wasabipesto@users.noreply.github.com>
2023-04-09 22:29:27 -0400

    wipe schema

--- schema/040623_create_history.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS History;
-CREATE TABLE IF NOT EXISTS History (
-    id SERIAL PRIMARY KEY,
-    start_time TIMESTAMP NOT NULL,
-    end_time TIMESTAMP NOT NULL,
-    username TEXT NOT NULL,
-    searched_detailed NUMERIC NOT NULL DEFAULT 0,
-    searched_niceonly NUMERIC NOT NULL DEFAULT 0
-);
